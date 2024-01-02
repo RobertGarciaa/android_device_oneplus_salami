b45b04b34d419ce2720c1fc04a22e3b1c1a1899f
RobertGarciaa <chae0218@naver.com>
2024-01-02 16:35:31 +0900

    salami: Bring up PixelOS
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- aosp_salami.mk
+++ aosp_salami.mk
@@ -11,14 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from salami device
 $(call inherit-product, device/oneplus/salami/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_salami
+PRODUCT_NAME := aosp_salami
 PRODUCT_DEVICE := salami
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := CPH2449
+
+TARGET_GAPPS_ARCH := arm64
 
 PRODUCT_SYSTEM_NAME := $(PRODUCT_MODEL)
 PRODUCT_SYSTEM_DEVICE := OP594DL1
