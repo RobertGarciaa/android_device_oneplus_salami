00f99302715cef7eaa5d0e623f8b7afe13a6ee83
RobertGarciaa <chae0218@naver.com>
2025-11-26 13:35:09 +0900

    salami: Include mindthegapps
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- lineage_salami.mk
+++ lineage_salami.mk
@@ -14,6 +14,11 @@ $(call inherit-product, device/oneplus/salami/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Call mindthegapps
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+ 
+WITH_GAPPS := true
+
 PRODUCT_NAME := lineage_salami
 PRODUCT_DEVICE := salami
 PRODUCT_MANUFACTURER := OnePlus
