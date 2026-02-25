7fa05d0ebb47fc024c33052d15c1fc110cf17f58
RobertGarciaa <chae0218@naver.com>
2026-02-25 14:33:18 +0900

    salami: Update from COS 11 H.11
    Convert from OOS to COS
    
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- lineage_salami.mk
+++ lineage_salami.mk
@@ -18,14 +18,14 @@ PRODUCT_NAME := lineage_salami
 PRODUCT_DEVICE := salami
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := CPH2449
+PRODUCT_MODEL := PHB110
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi-user 16 BP2A.250605.015 1778208971263 release-keys" \
-    BuildFingerprint=OnePlus/CPH2449EEA/OP594DL1:16/TP1A.220905.001/T.R4T3.3a47df1-16a215b-168e4a6:user/release-keys \
-    DeviceName=OP594DL1 \
-    DeviceProduct=CPH2449 \
-    SystemDevice=OP594DL1 \
-    SystemName=CPH2449
+    BuildDesc="qssi-user 16 BP2A.250605.015 1775718405982 release-keys" \
+    BuildFingerprint=OnePlus/PHB110/OP591BL1:16/TP1A.220905.001/T.25aae10-258862-26dfb8:user/release-keys \
+    DeviceName=OP591BL1 \
+    DeviceProduct=PHB110 \
+    SystemDevice=OP591BL1 \
+    SystemName=PHB110
