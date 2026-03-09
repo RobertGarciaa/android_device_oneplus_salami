e24f06b4b07bc8ccdcbbb16bb1e0b9f84874a2c3
RobertGarciaa <chae0218@naver.com>
2026-03-09 10:48:38 +0900

    salami: Support E-sim for chinese model
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- device.mk
+++ device.mk
@@ -38,6 +38,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    FrameworksResEuicc_CN \
     FrameworksResEuicc_EU \
     FrameworksResEuicc_NA \
     KeyHandlerResTarget \
